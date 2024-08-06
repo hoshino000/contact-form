6edd770934c36d06c0552f293e5c9c91b2c41121
hoshino0 <hhoshino273@gmail.com>
2024-08-06 22:22:16 +0900

    1th commit

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -11,6 +11,9 @@ class ContactController extends Controller
         return view('index');
     }
 
-    public function confirm()
-    
+    public function confirm(Request $request)
+    {
+        $contact = $request->only(['name', 'email', 'tel', 'content']);
+        return view('confirm', compact('contact'));
+    }
 }
